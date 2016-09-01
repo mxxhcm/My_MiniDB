dcd8f833f497649baf2310329088360330251c42
ThiefLock <402668446@qq.com>
2016-09-01 16:51:14 +0800

    代码框架
    函数声明代码（未实现）

---
+++ HandleData.cpp
@@ -0,0 +1,13 @@
+#include "HandleData.h"
+void insertData(string tableName, vector<pairData> _dataPair)
+{
+
+}
+void updateData(string tableName, vector<pairData> updata_data,string where_command)
+{
+
+}
+void deleteData(string tableName, string where_command)
+{
+
+}
---
+++ HandleTable.cpp
@@ -0,0 +1,13 @@
+#include "HandleTable.h"
+void createTable(string tableName, vector<fieldType> field)
+{
+
+}
+void dropTable(string tableName)
+{
+
+}
+void addField(string tableName, fieldType newField)
+{
+
+}
